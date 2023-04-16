76c2570cc6cdb03842ec3f61753b98132c71c72e
BjornRodin <bjornrodin@gmail.com>
2023-04-16 16:07:05 +0000

    Add SCOPE and assigning credentials

--- run.py
+++ run.py
@@ -1,2 +1,21 @@
+"""
+Imported dependencies such as gspread to be able to access the spreadsheet
+"""
 import gspread
 from google.oauth2.service_account import Credentials
+
+"""
+Assigning credentials from the API's to access the spreadsheet and it's content
+"""
+SCOPE = [
+    "https://www.googleapis.com/auth/spreadsheets",
+    "https://www.googleapis.com/auth/drive.file",
+    "https://www.googleapis.com/auth/drive"
+    ]
+
+CREDS = Credentials.from_service_account_file('creds.json')
+SCOPED_CREDS = CREDS.with_scopes(SCOPE)
+GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
+SHEET = GSPREAD_CLIENT.open('hangman')
+
+words = SHEET.worksheet('words')
